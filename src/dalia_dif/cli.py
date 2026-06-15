878df9c968738706338ea1842939863704b61eed
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-06-15 17:03:11 +0200

    Remove double spaces

--- src/dalia_dif/cli.py
+++ src/dalia_dif/cli.py
@@ -72,7 +72,7 @@ def lint(location: Path) -> None:
 
     count = 0
     for path in location.glob("*.csv"):
-        path.write_text(path.read_text().replace(" ", " "))  # noqa:RUF001
+        path.write_text(path.read_text().replace(" ", " ").replace("  ", " ").rstrip() + "\n")  # noqa:RUF001
         df = pd.read_csv(path, sep=",")
         if DIF_HEADER_ID not in df.columns:
             click.secho(f"missing column {DIF_HEADER_ID} in {path}")
